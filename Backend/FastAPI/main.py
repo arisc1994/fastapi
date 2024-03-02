82f5d51f1be9dca576c3b86684e31c66b8fa0ad9
Aris Cahuana <ariscahuana@outlook.com>
2024-03-02 17:59:38 -0500

    chore(Web): add instructions as comments

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -9,3 +9,9 @@ async def root():
 @app.get("/url")
 async def url():
   return {"url": "https://arisdev.com/python"}
+
+# Inicia  el servidor en localhost:8000
+# Detener el servidor: Ctrl+C
+
+# Documentación con Swagger: http://127.0.0.1:8000/docs
+# Documentación con Swagger: http://127.0.0.1:8000/redoc
